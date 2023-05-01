7c94987526189d72b48760fcd9bc65840a603c3c
rakrishna <rakrishna@sandvine.com>
2023-05-01 05:25:58 -0400

    Latest MPLS HEader from upstream

--- src/common/Network/Packet/MPLSHeader.h
+++ src/common/Network/Packet/MPLSHeader.h
@@ -1,12 +1,9 @@
 /*
 Copyright (c) 2015-2015 Cisco Systems, Inc.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +13,7 @@ limitations under the License.
 
 #ifndef _MPLS_HEADER_H_
 #define _MPLS_HEADER_H_
+
 #include "PacketHeaderBase.h"
 #include "EthernetHeader.h"
 #include <common/BigEndianBitManipulation.h>
@@ -36,10 +34,11 @@ limitations under the License.
  *	TTL:    Time to Live, 8 bits
  */
 
-#define MPLS_LS_LABEL_MASK      0xFFFFF000
-#define MPLS_LS_LABEL_SHIFT     12
-#define MPLS_LS_TC_MASK         0x00000E00
-#define MPLS_LS_TC_SHIFT        9
+class MPLSHeader
+{
+    uint32_t myTag;
+
+public:
     MPLSHeader()
     {
         myTag = 0;
@@ -66,6 +65,7 @@ limitations under the License.
         setMaskBit32(tempTag, 0, 19, label);
         this->myTag = PKT_HTONL(tempTag);
     }
+
     uint8_t getTc()
     {
         return getMaskBit32(PKT_NTOHL(this->myTag), 20, 22);
@@ -82,19 +82,31 @@ limitations under the License.
     {
         // return this->s;
         return (getMaskBit32(PKT_NTOHL(this->myTag), 23, 23) == 1);
+    }
+
+    void setBottomOfStack(bool s)
+    {
         uint32_t tempTag = PKT_NTOHL(this->myTag);
+        setMaskBit32(tempTag, 23, 23, s ? 1 : 0);
         this->myTag = PKT_HTONL(tempTag);
     }
 
-    uint8_t getTtl() {
+    uint8_t getTtl()
+    {
         return getMaskBit32(PKT_NTOHL(this->myTag), 24, 31);
     }
 
     void setTtl(uint8_t ttl)
     {
         uint32_t tempTag = PKT_NTOHL(this->myTag);
+        setMaskBit32(tempTag, 24, 31, ttl);
+        this->myTag = PKT_HTONL(tempTag);
+    }
+
     uint8_t *getPointer()
+    {
         return (uint8_t *)this;
     }
 };
+
 #endif // _MPLS_HEADER_H_
